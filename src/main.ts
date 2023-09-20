ca9f612f72d04c310b91350a577a62607cf91945
Shahor <shahor@shahor.fr>
2023-09-20 09:59:04 +0200

    Problem reproduction

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  /**
+   * We could attach a handler to `unhandledRejection` to handle this.
+   * But it seems NestJS already does that and exits the process as we should.
+   */
+  throw new Error('foobar');
 }
 bootstrap();
